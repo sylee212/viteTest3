210fdae327a18e79f8642b47f200acc831cd4d9d
Lee Sing Yuan <slee0163@student.monash.edu>
2025-01-07 15:49:48 +0800

    add required configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/vitetest3/",
+  plugins: [
+    react(),
+    ghPages(),
+  ],
 })
