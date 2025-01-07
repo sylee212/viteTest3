f1b1cfb58c8afdbca251494c6ed6740d068666c2
Lee Sing Yuan <slee0163@student.monash.edu>
2025-01-07 16:16:09 +0800

    tried removing the base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/viteTest3/",
+  base: "/",
   plugins: [
     react(),
     ghPages(),
