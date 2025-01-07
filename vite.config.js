04276bd8ffbad4542c5ae5a1e1a07a890b04c74a
Lee Sing Yuan <slee0163@student.monash.edu>
2025-01-07 16:16:52 +0800

    it worked, reverting back to correc tbase

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/viteTest3/",
   plugins: [
     react(),
     ghPages(),
